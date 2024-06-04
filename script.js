94ecdce3e8cf998ffdb5b3222da0a05336f465be
Dhruv Jain <dhruvjain0412@gmail.com>
2024-06-05 01:39:52 +0530

    Implemented ToDoList Array

--- script.js
+++ script.js
@@ -4,24 +4,29 @@ document.querySelector("#addButton").addEventListener("click", pushList);
 function pushList() {
     var toDo = document.querySelector("#addInput")
     var toDoItems = document.querySelector(".toDoItems")
-    toDoList =[]
+    console.log(toDoList)
     if (toDo.value !== "") {
+        toDoItems.innerHTML=""
         toDoList.push(toDo.value)
+        toDoList.reverse()
         toDoList.forEach(
             (item) => {
-                var parentDiv = document.querySelector(".toDoItems")
-                var newDiv = document.createElement("div");
-                newDiv.classList.add('toDoItem')
-                var sonDiv = document.createElement("div")
-                sonDiv.classList.add('toDoItemText')
-                var sonButton = document.createElement("button")
-                parentDiv.appendChild(newDiv);
-                newDiv.appendChild(sonDiv);
-                newDiv.appendChild(sonButton);
-                sonButton.textContent = "remove"
-                sonButton.classList.add("remove");
-                sonDiv.textContent = toDo.value;
+                var toDoItem = document.createElement("div");
+                toDoItem.classList.add('toDoItem')
+                toDoItems.appendChild(toDoItem);
+
+                var toDoItemText = document.createElement("div")
+                toDoItemText.classList.add('toDoItemText')
+                toDoItem.appendChild(toDoItemText);
+
+                var removeButton = document.createElement("button")
+                removeButton.classList.add("remove");
+                removeButton.textContent = "remove"
+                toDoItem.appendChild(removeButton);
+
+                toDoItemText.textContent = item;
             }
         )
     }
+    console.log(toDoList)
 }
