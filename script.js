c002fa4b5eaba4121f9fbc7030af6ed5e7972ba6
Dhruv Jain <dhruvjain0412@gmail.com>
2024-06-06 04:10:46 +0530

    Added Completed Tasks

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var toDoList = []
+var tickedList = []
 document.querySelector("#addButton").addEventListener("click", pushItem);
 
 document.addEventListener("keydown", (key) => {
@@ -20,8 +21,19 @@ function pushItem() {
     }
 }
 
-function removeItem() {
-    console.log(toDoList)
+function removeFromToDoItem() {
+    toDoList.splice(this.id, 1)
+    displayItems()
+}
+
+function removeFromTickedList() {
+    tickedList.splice(this.id, 1)
+    displayItems()
+}
+
+function tickItem() {
+    const textContent = toDoList[this.id]
+    tickedList.push(textContent)
     toDoList.splice(this.id, 1)
     displayItems()
 }
@@ -31,6 +43,9 @@ function displayItems() {
     toDoItems.innerHTML = ""
     toDoList.forEach(
         (item) => {
+
+            var toDoItems = document.querySelector(".toDoItems")
+
             var index = toDoList.findIndex(element => element === item)
 
             var toDoItem = document.createElement("div");
@@ -45,7 +60,41 @@ function displayItems() {
             removeButton.classList.add("remove");
             removeButton.id = index
             removeButton.innerHTML = `<i class="fa fa-trash-o" style="font-size:24px"></i>`
-            removeButton.addEventListener("click", removeItem)
+            removeButton.addEventListener("click", removeFromToDoItem)
+            toDoItem.appendChild(removeButton);
+
+            var tickButton = document.createElement("button")
+            tickButton.classList.add("tick")
+            tickButton.id = index
+            tickButton.innerHTML = `<i class="fa fa-check-square-o" aria-hidden="true" style= "font-size:24px"></i>`
+            tickButton.addEventListener("click", tickItem)
+            toDoItem.appendChild(tickButton)
+
+            toDoItemText.textContent = item;
+        }
+    )
+
+    tickedList.forEach(
+        (item) => {
+
+            var toDoItems = document.querySelector(".toDoItems")
+
+            var index = tickedList.findIndex(element => element === item)
+
+            var toDoItem = document.createElement("div");
+            toDoItem.classList.add('toDoItem')
+            toDoItems.appendChild(toDoItem);
+
+            var toDoItemText = document.createElement("div")
+            toDoItemText.classList.add('toDoItemText')
+            toDoItemText.classList.add('lineThrough')
+            toDoItem.appendChild(toDoItemText);
+
+            var removeButton = document.createElement("button")
+            removeButton.classList.add("remove");
+            removeButton.id = index
+            removeButton.innerHTML = `<i class="fa fa-trash-o" style="font-size:24px"></i>`
+            removeButton.addEventListener("click", removeFromTickedList)
             toDoItem.appendChild(removeButton);
 
             toDoItemText.textContent = item;
